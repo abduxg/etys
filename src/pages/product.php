421c3adaef7d44ba3eb73e453620f8ebd010c0b1
abduxg <abdussamedguzey58@gmail.com>
2021-08-30 00:31:15 +0300

    Update product.php

--- src/pages/product.php
+++ src/pages/product.php
@@ -1,1 +1,28 @@
 <?php
+if (isset($_GET["id"])){
+    $product_id = $_GET["id"];
+}
+else{
+    header("index.php");
+    exit();
+}
+
+$query = $database->prepare("SELECT * FROM products WHERE id=? LIMIT 1");
+$query->execute([$product_id]);
+$data = $query->fetch(PDO::FETCH_ASSOC);
+?>
+
+<table border="1">
+    <tr>
+        <td width="200">product name</td>
+        <td width="200">product image</td>
+        <td width="200">product price</td>
+    </tr>
+
+        <tr>
+            <td width="200"><?php echo $data["name"];?></td>
+            <td width="200"><img src="<?php echo $data["image"];?>" alt="<?php echo $data["name"]; ?>" width="200" height="auto"></td>
+            <td width="200"><?php echo $data["price"];?></td>
+
+<tr>
+</table>
